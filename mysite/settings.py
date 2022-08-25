989648d1ce610770e8fa709499fa80a1f4fd7e4b
Felipe Tavares <fotavares@gmail.coms>
2022-08-25 11:30:59 -0300

    Inclusão dos times importados de dsv

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'aa8&pw3fx9jl$8#*%9yng__jn_$*&5!q-hr9cg2d*ocq7d%&3-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bolaodagalera.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
